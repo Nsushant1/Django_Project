5ae803077f8947cc0627bfbecc746c8cd6e9d119
Sushant Neupane <nsushaant72@gmail.com>
2025-01-26 16:25:08 +0545

    Upadte on views

--- Blog/blogApp/views.py
+++ Blog/blogApp/views.py
@@ -15,6 +15,24 @@ def create_blog(request):
 
 def blog_list(request):
     blog=Blog.objects.all().order_by("-created_at")
-    return render(request, "", {"blogs":blog})
+    return render(request, "", {"blog":blog})
+
+def update_blog(request, blog_id):
+    blog = Blog.objects.get(id=blog_id)
+    if request.method == 'POST':
+        form = BlogForm(request.POST, instance=blog)
+        if form.is_valid():
+            form.save()
+            return redirect('blog_list')
+    else:
+        form = BlogForm(instance=blog)
+    return render(request, '', {'form': form})
+
+def delete_blog(request, blog_id):
+    blog = Blog.objects.get(id=blog_id)
+    if request.method == 'POST':
+        blog.delete()
+        return redirect('blog_list')
+    return render(request, '', {'blog': blog})
 
 
